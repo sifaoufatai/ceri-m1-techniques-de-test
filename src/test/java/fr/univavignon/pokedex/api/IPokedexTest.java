6b9fc5ec5b275129cb030f1f54fa01f6e023ad94
fatai <sifaoufatai@gmail.com>
2024-03-20 00:17:05 +0100

    POKEDEX TRAINER

--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -2,21 +2,17 @@ package fr.univavignon.pokedex.api;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 public class IPokedexTest {
 
     private IPokedex pokedex;
-
-
     private IPokemonMetadataProvider pokemonMetadataProvider;
     private IPokemonFactory pokemonFactory;
-
 
     @BeforeEach
     public void setUp() throws PokedexException {
@@ -40,56 +36,42 @@ public class IPokedexTest {
 
     @Test
     public void testSize() throws PokedexException {
-
         assertEquals(1, pokedex.size());
     }
-
-    @Test
-
-    public void testSizeEmptyPokedex() {
-
-        assertEquals(1, pokedex.size());
-    }
-
 
     @Test
     public void testAddPokemon() throws PokedexException {
         Pokemon pokemon2 = new Pokemon(
                 2, // index
-                "Bulbasaur", // name
-                45, //
-                49, //
-                49, //
-                12, //
-                35, //
-                5, //
-                80, //
-                0.5
+                "Ivysaur", // name
+                60, // cp
+                62, // hp
+                63, // dust
+                15, // candy
+                45, // capture
+                6, // attack
+                85, // defense
+                0.6 // stamina
         );
-
-
         assertEquals(2, pokedex.addPokemon(pokemon2));
-
     }
 
     @Test
     public void testGetPokemon() throws PokedexException {
-
         Pokemon actual = new Pokemon(
-                12,
-                "Bulbasaur",
-                45,
-                49,
-                49,
-                12,
-                35,
-                5,
-                80,
-                0.5
+                12, // index
+                "Charizard", // name
+                78, // cp
+                84, // hp
+                85, // dust
+                21, // candy
+                55, // capture
+                7, // attack
+                90, // defense
+                1.7 // stamina
         );
         pokedex.addPokemon(actual);
         Pokemon expected = pokedex.getPokemon(12);
-
         assertEquals(actual, expected);
     }
 
@@ -113,70 +95,59 @@ public class IPokedexTest {
             pokemonList.add(pokemon);
             pokedex1.addPokemon(pokemon);
         }
-
         assertEquals(pokemonList, pokedex1.getPokemons());
     }
 
     @Test
-
     public void testGetPokemonsSorted() throws PokedexException {
         List<Pokemon> pokemonList2 = new ArrayList<>();
         Pokedex pokedex2 = new Pokedex(pokemonMetadataProvider, pokemonFactory);
-
         Pokemon pokemon1 = new Pokemon(
-                1,
-                "A",
-                45,
-                49,
-                49,
-                12,
-                35,
-                5,
-                80,
-                0.5
+                1, // index
+                "Charmander", // name
+                39, // cp
+                40, // hp
+                41, // dust
+                12, // candy
+                45, // capture
+                4, // attack
+                65, // defense
+                0.6 // stamina
         );
         pokemonList2.add(pokemon1);
-
         Pokemon pokemon2 = new Pokemon(
-                1,
-                "B",
-                45,
-                49,
-                49,
-                12,
-                35,
-                5,
-                80,
-                0.5
+                2, // index
+                "Charmeleon", // name
+                58, // cp
+                62, // hp
+                63, // dust
+                15, // candy
+                45, // capture
+                6, // attack
+                85, // defense
+                0.6 // stamina
         );
         pokemonList2.add(pokemon2);
         Pokemon pokemon3 = new Pokemon(
-                3,
-                "B",
-                45,
-                49,
-                49,
-                12,
-                35,
-                5,
-                80,
-                0.5
+                3, // index
+                "Charizard", // name
+                78, // cp
+                84, // hp
+                85, // dust
+                21, // candy
+                55, // capture
+                7, // attack
+                90, // defense
+                1.7 // stamina
         );
         pokemonList2.add(pokemon3);
-
-
         pokedex2.addPokemon(pokemon1);
         pokedex2.addPokemon(pokemon2);
         pokedex2.addPokemon(pokemon3);
-
-        // Triez les deux listes par ordre alphabétique des noms
+        // Tri des deux listes par ordre alphabétique des noms
         Comparator<Pokemon> comparator = Comparator.comparing(Pokemon::getName);
         pokemonList2.sort(comparator);
-
         List<Pokemon> sortedPokemons = pokedex2.getPokemons(comparator);
-
-
         assertEquals(pokemonList2, sortedPokemons);
-        System.out.println("liste sorted ");
     }
 }
