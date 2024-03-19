0af84476c52e2251efc30db715ce779c8f95b947
fatai <sifaoufatai@gmail.com>
2024-03-19 17:10:56 +0100

    Ipokedex

--- src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexFactoryTest.java
@@ -19,13 +19,15 @@ public class IPokedexFactoryTest {
 
         metadataProvider = new PokemonMetadataProvider();
         pokemonFactory = new PokemonFactory();
+        //Pokemon p1 = new Pokemon(0, "fata", 12, 12, 4, 3, 12, 12, 12.0)
+        //Pokemon p1 = new Pokemon(1, "fata", 12, 12, 4, 3, 12, 12, 12.0)
     }
 
     @Test
     public void testCreatePokedex() {
 
         IPokedex createdPokedex = pokedexFactory.createPokedex(metadataProvider, pokemonFactory);
-        Pokedex pokedex = new Pokedex(metadataProvider, pokemonFactory);
+
 
         assertNotNull(createdPokedex);
 
--- src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
+++ src/test/java/fr/univavignon/pokedex/api/IPokedexTest.java
@@ -1,119 +1,71 @@
 package fr.univavignon.pokedex.api;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
+import org.junit.Before;
+import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class IPokedexTest {
 
     private IPokedex pokedex;
-    private PokemonMetadata pokemonMetadata;
 
-    @BeforeEach
-    public void setUp() throws PokedexException {
-        pokedex = mock(IPokedex.class);
-        pokemonMetadata = new PokemonMetadata(1, "Bulbasaur", 45, 49, 49);
+    @Before
+    public void setUp() {
+
+        IPokemonMetadataProvider metadataProvider = new PokemonMetadataProvider();
+        IPokemonFactory pokemonFactory = new PokemonFactory();
+        pokedex = new Pokedex(metadataProvider, pokemonFactory);
     }
 
     @Test
-    public void testSize() throws PokedexException {
-        when(pokedex.size()).thenReturn(1);
+    public void testSize() {
+        assertEquals(0, pokedex.size());
+    }
+
+    @Test
+    public void testAddPokemon() {
+        Pokemon pokemon = new Pokemon(1, "Bulbasaur", 45, 49, 126, 4000, 25, 25, 1, 3.0);
+        int index = pokedex.addPokemon(pokemon);
+        assertEquals(1, index);
         assertEquals(1, pokedex.size());
     }
 
     @Test
-
-    public void testSizeEmptyPokedex() {
-        when(pokedex.size()).thenReturn(0);
-        assertEquals(0, pokedex.size());
-    }
-
-
-    @Test
-    public void testAddPokemon() throws PokedexException {
-        Pokemon pokemon = new Pokemon(
-                1, // index
-                "Bulbasaur", // name
-                45, //
-                49, //
-                49, //
-                12, //
-                35, //
-                5, //
-                80, //
-                0.5
-        );
-        when(pokedex.addPokemon(pokemon)).thenReturn(1);
-        assertEquals(1, pokedex.addPokemon(pokemon));
-        when(pokedex.addPokemon(pokemon)).thenReturn(1);
-    }
-
-    @Test
     public void testGetPokemon() throws PokedexException {
-        Pokemon expectedPokemon = new Pokemon(
-                1,
-                "Bulbasaur",
-                45,
-                49,
-                49,
-                12,
-                35,
-                5,
-                80,
-                0.5
-        );
-        when(pokedex.getPokemon(1)).thenReturn(expectedPokemon);
-        assertEquals(expectedPokemon, pokedex.getPokemon(1));
+        Pokemon pokemon = new Pokemon(1, "Bulbasaur", 45, 49, 126, 4000, 25, 25, 1, 3.0);
+        pokedex.addPokemon(pokemon);
+        Pokemon retrievedPokemon = pokedex.getPokemon(1);
+
+        org.junit.Assert.assertEquals(pokemon, retrievedPokemon);
+
+    }
+
+
+    @Test
+    public void testGetPokemons() {
+        List<Pokemon> pokemonList = pokedex.getPokemons();
+        assertEquals(0, pokemonList.size());
     }
 
     @Test
-    public void testGetPokemons() throws PokedexException {
+    public void testGetPokemonsSorted() {
         List<Pokemon> pokemonList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            Pokemon pokemon = new Pokemon(
-                    i + 1,
-                    "Bulbasaur",
-                    45,
-                    49,
-                    49,
-                    12,
-                    35,
-                    5,
-                    80,
-                    0.5
-            );
-            pokemonList.add(pokemon);
-        }
-        when(pokedex.getPokemons()).thenReturn(pokemonList);
-        assertEquals(pokemonList, pokedex.getPokemons());
-    }
 
-    @Test
-    public void testGetPokemonsSorted() throws PokedexException {
-        List<Pokemon> pokemonList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            Pokemon pokemon = new Pokemon(
-                    i + 1,
-                    "Math",
-                    45,
-                    49,
-                    49,
-                    12,
-                    35,
-                    5,
-                    80,
-                    0.5
-            );
-            pokemonList.add(pokemon);
-        }
+        pokemonList.add(new Pokemon(1, "Bulbasaur", 45, 49, 126, 4000, 25, 25, 3, 2.5));
+        pokemonList.add(new Pokemon(2, "Ivysaur", 60, 62, 151, 8000, 50, 25, 3 ,2.5));
+        pokemonList.add(new Pokemon(3, "Venusaur", 80, 82, 182, 12000, 75, 25, 3 , 2.5));
+
+
         Comparator<Pokemon> comparator = Comparator.comparing(Pokemon::getName);
         pokemonList.sort(comparator);
-        when(pokedex.getPokemons(comparator)).thenReturn(pokemonList);
-        assertEquals(pokemonList, pokedex.getPokemons(comparator));
+
+        List<Pokemon> sortedPokemons = pokedex.getPokemons(comparator);
+        org.junit.Assert.assertEquals(pokemonList, sortedPokemons);
+
     }
 }
